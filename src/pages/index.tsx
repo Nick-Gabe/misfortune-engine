886864870893de7f9d4edca83ccd05fd74519e4a
Nick Gabe <nicolasgabrielctt@gmail.com>
2024-10-26 10:28:30 -0300

    Installs framer, tailwind and creates routing

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./Home";
+import { Room } from "./Room";
+
+export const Pages = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/room/:id" element={<Room />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
