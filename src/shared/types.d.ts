a5daf98df31ec5e58e7b4aadbb36f0f839872478
Nick Gabe <nicolasgabrielctt@gmail.com>
2024-10-27 10:15:55 -0300

    installs openai and creates useAI hook

--- src/shared/types.d.ts
+++ src/shared/types.d.ts
@@ -1,15 +1,38 @@
 type User = {
   name: string;
   id: string;
+  answer: string | null;
+  points: number;
 }
 
 type GameMode = "versus" | "coop";
 
-type RoomScreen = "waitingRoom" | "decidingMisfortune";
+type RoomScreen = "waitingRoom" | "decidingMisfortune" | "misfortuneDecided" | "outcomeShowcase" | "leaderboard";
 
 type Room = {
   id: string;
   players: User[];
   gameMode: GameMode;
   screen: RoomScreen;
+  currentMisfortune?: {
+    announcement: string;
+    content: string;
+  };
+  currentShowcase?: {
+    user: User;
+    outcome: {
+      points: number;
+      content: string;
+    } | null
+  };
+}
+
+type OpenAIResponse = {
+  choices: {
+    index: number
+    message: {
+      role: string;
+      content: string;
+    };
+  }[];
 }
---
+++ src/shared/useAi.ts
@@ -0,0 +1,58 @@
+import { useMutation, useQuery } from "react-query";
+
+export const useAiQuery = ({
+  messages,
+  user,
+  room,
+}: {
+  messages: {
+    role: "system" | "user" | "assistant";
+    content: string;
+  }[];
+  user: User;
+  room: Room | null;
+}) => useQuery({
+  refetchOnWindowFocus: false,
+  refetchOnMount: false,
+  refetchOnReconnect: false,
+  refetchInterval: false,
+  refetchIntervalInBackground: false,
+  cacheTime: 0,
+  enabled: user.id === room?.players[0].id,
+  queryFn: async (): Promise<OpenAIResponse> =>
+    await (
+      await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-4o",
+          messages: messages,
+          temperature: 0.8,
+        }),
+      })
+    ).json(),
+});
+
+export const useAiMutation = () => useMutation({
+  mutationFn: async (messages: {
+    role: "system" | "user" | "assistant";
+    content: string;
+  }[]): Promise<OpenAIResponse> =>
+    await (
+      await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-4o-mini",
+          messages: messages,
+          temperature: 0.8,
+        }),
+      })
+    ).json(),
+});
